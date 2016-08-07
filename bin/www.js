4430c3a6497ae6b71a33b34f4f8bcd82c560fe62
唐轶 <tangyi057@1caifu.com>
2016-08-07 12:03:49 +0800

    修改

--- bin/www.js
+++ bin/www.js
@@ -3,6 +3,6 @@
  */
 var http=require('http');
 var app=require('../app.js')
-http.createServer(app).listen(8400).on('listening',function(){
+http.createServer(app).listen(80).on('listening',function(){
     console.log('Listening on port 8400');
 });
